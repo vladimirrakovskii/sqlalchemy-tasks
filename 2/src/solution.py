10a8f3adba3ac559758db6279f75f3fc40cdb93b
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:49:05 +0300

    Update solution.py

--- 2/src/solution.py
+++ 2/src/solution.py
@@ -9,7 +9,19 @@ engine = create_engine(os.environ["DATABASE_URL"])
 
 
 # BEGIN (write your solution here)
+class Base(DeclarativeBase):
+    pass
 
+class Book(Base):
+    __tablename__ = "book"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(256), unique=True)
+    author: Mapped[str] = mapped_column(String(128))
+    published_date: Mapped[datetime.date] = mapped_column(Date)
+    pages: Mapped[int] = mapped_column()
+    genre: Mapped[str] = mapped_column(String(64))
+    rating: Mapped[float] = mapped_column()
 # END
 
 Base.metadata.drop_all(engine)
