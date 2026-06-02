11d6de0e193bc8f399dfcef6e3c2336307135f7e
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:48:25 +0300

    Update solution.py

--- 1/src/solution.py
+++ 1/src/solution.py
@@ -6,5 +6,6 @@ load_dotenv()
 
 
 # BEGIN (write your solution here)
-
+def create_db_engine(db_url, echo=False, pool_size=5, max_overflow=10):
+    return create_engine(url=db_url, echo=echo, pool_size=pool_size, max_overflow=max_overflow)
 # END
