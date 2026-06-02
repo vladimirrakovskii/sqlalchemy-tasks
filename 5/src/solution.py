fac233318c222cb4ea120cd464cb29949365f2b4
vladimirrakovskii <v.rakovscky@yandex.ru>
2026-06-02 13:51:24 +0300

    Update solution.py

--- 5/src/solution.py
+++ 5/src/solution.py
@@ -15,5 +15,13 @@ Base.metadata.create_all(engine)
 
 
 # BEGIN (write your solution here)
-
+def get_movies_with_directors(sess):
+    movies_list = []
+    query = select(Movie, Director.name).join(Movie.director).order_by(Movie.title)
+    result = sess.execute(query)
+    for movie, director_name in result:
+        movies_list.append(f"{movie.title} by {director_name}, "
+                           f"released on {movie.release_date}, duration: {movie.duration} min,"
+                           f" genre: {movie.genre}, rating: {movie.rating}")
+    return movies_list
 # END
